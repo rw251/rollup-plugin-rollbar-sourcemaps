72707ebab6ee121dab47674742124c21a8832225
Richard Williams <1234richardwilliams@gmail.com>
2023-04-05 15:51:45 +0100

    Fix lint issue

--- src/rollup-plugin-rollbar-sourcemaps.js
+++ src/rollup-plugin-rollbar-sourcemaps.js
@@ -2,31 +2,30 @@
 import FormData from 'form-data';
 import { ROLLBAR_ENDPOINT } from './constants';
 
-const submitSourcemaps = ({ rollbarEndpoint, silent, form }) =>
-  new Promise((resolve, reject) => {
-    form.submit(rollbarEndpoint, (err, response) => {
-      if (err) return reject(err);
-      if (response.statusCode === 200) {
-        if (!silent) {
-          console.log('Sourcemaps successfully uploaded to Rollbar.');
-        }
-        return resolve();
+const submitSourcemaps = ({ rollbarEndpoint, silent, form }) => new Promise((resolve, reject) => {
+  form.submit(rollbarEndpoint, (err, response) => {
+    if (err) return reject(err);
+    if (response.statusCode === 200) {
+      if (!silent) {
+        console.log('Sourcemaps successfully uploaded to Rollbar.');
       }
-      let body = [];
-      return response
-        .on('data', (chunk) => {
-          body.push(chunk);
-        })
-        .on('end', () => {
-          body = Buffer.concat(body).toString();
-          console.log(
-            'Sourcemaps failed to upload to Rollbar. The response from the api call is:'
-          );
-          console.log(body);
-          resolve();
-        });
-    });
+      return resolve();
+    }
+    let body = [];
+    return response
+      .on('data', (chunk) => {
+        body.push(chunk);
+      })
+      .on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log(
+          'Sourcemaps failed to upload to Rollbar. The response from the api call is:'
+        );
+        console.log(body);
+        resolve();
+      });
   });
+});
 
 export default function rollbarSourcemaps({
   accessToken,
@@ -45,9 +44,10 @@ export default function rollbarSourcemaps({
     },
     name: 'rollup-plugin-rollbar-sourcemaps',
     async writeBundle(bundle) {
-      const entryWithMap = Object.entries(bundle)
-        .filter((entry) => entry.length > 1 && entry[1])
-        .find((entry) => entry[1].map);
+      const possibleEntries = Object.entries(bundle).filter(
+        (entry) => entry.length > 1 && entry[1]
+      );
+      const entryWithMap = possibleEntries.find((entry) => entry[1].map);
       if (!entryWithMap) {
         if (!silent) {
           this.warn({
