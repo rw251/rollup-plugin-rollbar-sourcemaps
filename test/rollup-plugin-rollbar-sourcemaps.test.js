a08c1475adf0247309d4eb7ca42ea6ea5aeddbea
Richard Williams <1234richardwilliams@gmail.com>
2023-11-08 11:53:55 +0000

    Updated tests

--- test/rollup-plugin-rollbar-sourcemaps.test.js
+++ test/rollup-plugin-rollbar-sourcemaps.test.js
@@ -5,7 +5,7 @@ import { ROLLBAR_ENDPOINT } from '../src/constants';
 
 process.chdir(join(__dirname, 'fixtures'));
 
-function expectWarnings (warnings) {
+function expectWarnings(warnings) {
   let warningIndex = 0;
   return jest.fn((warning) => {
     if (warningIndex >= warnings.length) {
@@ -22,10 +22,10 @@ function expectWarnings (warnings) {
 
 const expectNoWarnings = expectWarnings([]);
 
-describe('rollup-plugin-rollbar-deploy', function() {
+describe('rollup-plugin-rollbar-deploy', function () {
   let rollupPlugin;
   let options;
-  beforeEach(function() {
+  beforeEach(function () {
     options = {
       accessToken: 'aaaabbbbccccddddeeeeffff00001111',
       baseUrl: '//test.com',
@@ -35,29 +35,33 @@ describe('rollup-plugin-rollbar-deploy', function() {
     rollupPlugin = rollbarSourcemaps(options);
   });
 
-  describe('constructor', function() {
-    it('should return an instance with the correct name', function() {
+  describe('constructor', function () {
+    it('should return an instance with the correct name', function () {
       expect(rollupPlugin.name).toBe('rollup-plugin-rollbar-sourcemaps');
     });
 
-    it('should set options', function() {
+    it('should set options', function () {
       const localOptions = { ...this.options, silent: true };
       const localPlugin = rollbarSourcemaps(localOptions);
       expect(localPlugin).not.toBe(undefined);
-      expect(localPlugin.localProps).toEqual(expect.objectContaining({ silent: true }));
+      expect(localPlugin.localProps).toEqual(
+        expect.objectContaining({ silent: true })
+      );
     });
 
-    it('should default silent to false', function() {
-      expect(rollupPlugin.localProps).toEqual(expect.objectContaining({ silent: false }));
+    it('should default silent to false', function () {
+      expect(rollupPlugin.localProps).toEqual(
+        expect.objectContaining({ silent: false })
+      );
     });
 
-    it('should default rollbarEndpoint to ROLLBAR_ENDPOINT constant', function() {
+    it('should default rollbarEndpoint to ROLLBAR_ENDPOINT constant', function () {
       expect(rollupPlugin.localProps).toEqual(
         expect.objectContaining({ rollbarEndpoint: ROLLBAR_ENDPOINT })
       );
     });
 
-    it('should access string value for rollbarEndpoint', function() {
+    it('should access string value for rollbarEndpoint', function () {
       const customEndpoint = 'https://api.rollbar.custom.com/api/1/sourcemap';
       const localOptions = { ...this.options, rollbarEndpoint: customEndpoint };
       const localPlugin = rollbarSourcemaps(localOptions);
@@ -67,28 +71,28 @@ describe('rollup-plugin-rollbar-deploy', function() {
     });
   });
 
-  describe('apply', function() {
-    it('should call submitSourcemaps', async function() {
+  describe('apply', function () {
+    it('should call submitSourcemaps', async function () {
       const mockSubmitSourcemaps = jest.fn();
       rollbarSourcemaps.__Rewire__('submitSourcemaps', mockSubmitSourcemaps);
 
       const bundle = await rollup({
         input: 'index.js',
-        plugins: [rollbarSourcemaps(options)]
+        plugins: [rollbarSourcemaps(options)],
       });
       await bundle.write({ dir: 'output', format: 'umd', sourcemap: true });
       expect(mockSubmitSourcemaps.mock.calls.length).toBe(1);
       rollbarSourcemaps.__ResetDependency__('submitSourcemaps');
     });
 
-    it('should call submitSourcemaps after writeBundle hook', async function() {
+    it('should call submitSourcemaps after writeBundle hook', async function () {
       const mockSubmitSourcemaps = jest.fn();
       rollbarSourcemaps.__Rewire__('submitSourcemaps', mockSubmitSourcemaps);
 
       expect(mockSubmitSourcemaps.mock.calls.length).toBe(0);
       const bundle = await rollup({
         input: 'index.js',
-        plugins: [rollbarSourcemaps(options)]
+        plugins: [rollbarSourcemaps(options)],
       });
       expect(mockSubmitSourcemaps.mock.calls.length).toBe(0);
       await bundle.write({ dir: 'output', format: 'umd', sourcemap: true });
@@ -96,55 +100,59 @@ describe('rollup-plugin-rollbar-deploy', function() {
       rollbarSourcemaps.__ResetDependency__('submitSourcemaps');
     });
 
-    it('should call submitSourcemaps with the correct parameters', async function() {
+    it('should call submitSourcemaps with the correct parameters', async function () {
       const mockSubmitSourcemaps = jest.fn();
       rollbarSourcemaps.__Rewire__('submitSourcemaps', mockSubmitSourcemaps);
 
       const bundle = await rollup({
         input: 'index.js',
-        plugins: [rollbarSourcemaps(options)]
+        plugins: [rollbarSourcemaps(options)],
       });
       await bundle.write({ dir: 'output', format: 'umd', sourcemap: true });
       expect(mockSubmitSourcemaps.mock.calls.length).toBe(1);
       expect(mockSubmitSourcemaps.mock.calls[0][0].silent).toBe(false);
-      expect(mockSubmitSourcemaps.mock.calls[0][0].rollbarEndpoint).toBe(ROLLBAR_ENDPOINT);
+      expect(mockSubmitSourcemaps.mock.calls[0][0].rollbarEndpoint).toBe(
+        ROLLBAR_ENDPOINT
+      );
       rollbarSourcemaps.__ResetDependency__('submitSourcemaps');
     });
 
-    it('should call correctly without warnings', async function() {
+    it('should call correctly without warnings', async function () {
       const mockSubmitSourcemaps = jest.fn();
       rollbarSourcemaps.__Rewire__('submitSourcemaps', mockSubmitSourcemaps);
 
       const bundle = await rollup({
         input: 'index.js',
         onwarn: expectNoWarnings,
-        plugins: [rollbarSourcemaps(options)]
+        plugins: [rollbarSourcemaps(options)],
       });
       await bundle.write({ dir: 'output', format: 'umd', sourcemap: true });
       rollbarSourcemaps.__ResetDependency__('submitSourcemaps');
     });
 
-    it('should warn if not sourcemaps found', async function() {
-      const onwarn = expectWarnings([{
-        code: 'PLUGIN_WARNING',
-      }]);
+    it('should warn if not sourcemaps found', async function () {
+      const onwarn = expectWarnings([
+        {
+          code: 'PLUGIN_WARNING',
+        },
+      ]);
       const bundle = await rollup({
         input: 'index.js',
         onwarn,
-        plugins: [rollbarSourcemaps(options)]
+        plugins: [rollbarSourcemaps(options)],
       });
       await bundle.write({ dir: 'output', format: 'umd' });
       expect(onwarn.mock.calls.length).toBe(1);
     });
 
-    it('shouldn\'t call submitSourcemaps if not sourcemaps found', async function() {
+    it("shouldn't call submitSourcemaps if not sourcemaps found", async function () {
       const mockSubmitSourcemaps = jest.fn();
       rollbarSourcemaps.__Rewire__('submitSourcemaps', mockSubmitSourcemaps);
 
       const bundle = await rollup({
         input: 'index.js',
         onwarn: () => {}, // suppress the warning that we know we'll get
-        plugins: [rollbarSourcemaps(options)]
+        plugins: [rollbarSourcemaps(options)],
       });
       await bundle.write({ dir: 'output', format: 'umd' });
       expect(mockSubmitSourcemaps.mock.calls.length).toBe(0);
